57853fcb0908c55a30f980aaa5b6a4174b2db57c
Conner Cochrane <cccochrane1@knights.ucf.edu>
2022-01-18 16:38:55 -0500

    adjusting for the API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 var nasaPictures = JSON.parse(localStorage.getItem("nasaPicture"))
+const APIKey = "6AIGbj4jHFCcD7ChEiWMreQx3wbVgxowEsvyWVjz"
+var apiURL = `https://api.nasa.gov/planetary/apod?api_key=6AIGbj4jHFCcD7ChEiWMreQx3wbVgxowEsvyWVj`
+
+
+
+var titleInput = document.getElementById("title")
+var dateInput = document.getElementById("date")
+var descriptionInput = document.getElementById("title")
+
+
+var searchImage = function (data) {
+    event.preventDefault()
+    $("#main-image").empty()
+    var inputNasaVal = titleInput.value
+    
+}
